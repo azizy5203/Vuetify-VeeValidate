004d6b409bffb14bec9720aaf44c427e920ee447
Youssef Abdulaziz <azizy5203@gmail.com>
2024-05-20 14:27:32 +0300

    refactor : add custom expansion table route

--- src/layouts/default/Admin.vue
+++ src/layouts/default/Admin.vue
@@ -2,12 +2,11 @@
 import { ref, onMounted, inject } from "vue";
 import { toast } from "vue3-toastify";
 
-
-const vuetify = inject('vuetify')
+const vuetify = inject("vuetify");
 
 //sidebar expand switch
 const drawer = ref(false);
-onMounted(() => drawer.value = !vuetify.display.mobile.value)
+onMounted(() => (drawer.value = !vuetify.display.mobile.value));
 
 // sidebar dropdown items
 const cruds = [
@@ -17,37 +16,89 @@ const cruds = [
   ["Delete", "mdi-delete"],
 ];
 
-
 //footer items
 const links = ["Home", "About Us", "Team", "Services", "Blog", "Contact Us"];
 </script>
 
 <template>
-  <v-navigation-drawer class="pa-2 pb-5" v-model="drawer" border="0" color="hsl(225 50% 90% /1)">
-    <v-list nav class="pa-0" color="white" variant="tonal" active-color="blue" density="comfortable">
-      <v-list-item title="Home" value="home" to="/" exact prepend-icon="mdi-home" />
-      <v-list-item title="Try" value="about" to="/try" prepend-icon="mdi-test-tube" />
-      <v-list-item title="Expand" value="expand" to="/expand" prepend-icon="mdi-arrow-expand-vertical" />
+  <v-navigation-drawer
+    class="pa-2 pb-5"
+    v-model="drawer"
+    border="0"
+    color="hsl(225 50% 90% /1)"
+  >
+    <v-list
+      nav
+      class="pa-0"
+      color="white"
+      variant="tonal"
+      active-color="blue"
+      density="comfortable"
+    >
+      <v-list-item
+        title="Home"
+        value="home"
+        to="/"
+        exact
+        prepend-icon="mdi-home"
+      />
+      <v-list-item
+        title="Try"
+        value="about"
+        to="/try"
+        prepend-icon="mdi-test-tube"
+      />
+      <v-list-item
+        title="Expand"
+        value="expand"
+        to="/expand"
+        prepend-icon="mdi-arrow-expand-vertical"
+      />
+      <v-list-item
+        title="Ready Expansion"
+        value="expansion"
+        to="/expansion"
+        prepend-icon="mdi-arrow-expand-vertical"
+      />
       <v-list-group value="Actions">
         <template #activator="{ props }">
           <v-list-item v-bind="props" title="Actions"></v-list-item>
         </template>
 
-        <v-list-item v-for="([title, icon], i) in cruds" :key="i" :value="title" :title="title" :prepend-icon="icon">
+        <v-list-item
+          v-for="([title, icon], i) in cruds"
+          :key="i"
+          :value="title"
+          :title="title"
+          :prepend-icon="icon"
+        >
         </v-list-item>
       </v-list-group>
     </v-list>
     <template #append>
-      <v-btn color="blue" prepend-icon="mdi-logout" to="/auth/login"><router-link to="/auth/login"
-          style="text-decoration: none">Logout</router-link></v-btn>
+      <v-btn color="blue" prepend-icon="mdi-logout" to="/auth/login"
+        ><router-link to="/auth/login" style="text-decoration: none"
+          >Logout</router-link
+        ></v-btn
+      >
     </template>
   </v-navigation-drawer>
   <!-- scroll-behavior="collapse elevate fade-image hide" -->
-  <v-app-bar density="default" :extended="false" :collapse="false" elevation="3"
-    image="https://picsum.photos/1920/1080?random&blur=10" scroll-behavior="hide" scroll-threshold="1000">
+  <v-app-bar
+    density="default"
+    :extended="false"
+    :collapse="false"
+    elevation="3"
+    image="https://picsum.photos/1920/1080?random&blur=10"
+    scroll-behavior="hide"
+    scroll-threshold="1000"
+  >
     <v-app-bar-title class="text-white"> App Title </v-app-bar-title>
     <template #prepend>
-      <v-app-bar-nav-icon color="white" @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+        color="white"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
     </template>
   </v-app-bar>
 
@@ -55,7 +106,14 @@ const links = ["Home", "About Us", "Team", "Services", "Blog", "Contact Us"];
     <router-view />
     <v-footer color="blue">
       <v-row justify="center" no-gutters>
-        <v-btn v-for="link in links" :key="link" color="white" variant="text" class="mx-2" rounded="xl">
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          variant="text"
+          class="mx-2"
+          rounded="xl"
+        >
           {{ link }}
         </v-btn>
         <v-col class="text-center mt-4 ma-auto" cols="12">
--- src/router/index.js
+++ src/router/index.js
@@ -1,87 +1,86 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    path: "/",
+    component: () => import("@/layouts/default/Default.vue"),
     children: [
       {
-        path: '',
-        name: 'Home',
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        path: "",
+        name: "Home",
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
       },
       {
-        path: '/try',
-        name: 'Try',
-        component: () => import('@/views/Try.vue'),
+        path: "/try",
+        name: "Try",
+        component: () => import("@/views/Try.vue"),
       },
       {
-        path: '/expand',
-        name: 'Expand',
-        component: () => import('@/views/Expand/Expand.vue'),
+        path: "/expand",
+        name: "Expand",
+        component: () => import("@/views/Expand/Expand.vue"),
       },
-      
+      {
+        path: "/expansion",
+        name: "ExpansionPanels",
+        component: () => import("@/views/Expand/ExpansionPanels.vue"),
+      },
     ],
-    meta:{
-      requiresAuth:true,
-    }
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
-    path: '/auth',
-    component: () => import('@/layouts/default/Default.vue'),
+    path: "/auth",
+    component: () => import("@/layouts/default/Default.vue"),
     children: [
       {
-        path: '/auth/login',
-        name: 'Login',
+        path: "/auth/login",
+        name: "Login",
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Login.vue'),
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/Login.vue"),
       },
       {
-        path: '/auth/signup',
-        name: 'Signup',
+        path: "/auth/signup",
+        name: "Signup",
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import('@/views/SignUp.vue'),
+        component: () => import("@/views/SignUp.vue"),
       },
     ],
-    meta:{
-      requiresAuth:false,
-    }
+    meta: {
+      requiresAuth: false,
+    },
   },
   {
-    path: '/animate',
-    name: 'Animate',
-    component: () => import('@/views/Animate.vue'),
+    path: "/animate",
+    name: "Animate",
+    component: () => import("@/views/Animate.vue"),
   },
   {
-    path: '/risk',
-    name: 'Risk',
-    component: () => import('@/views/Risks/RiskMatrix.vue'),
+    path: "/risk",
+    name: "Risk",
+    component: () => import("@/views/Risks/RiskMatrix.vue"),
   },
   {
-    path: '/collapse',
-    name: 'Collapse',
-    component: () => import('@/views/Collapse/CollapseContainer.vue'),
+    path: "/collapse",
+    name: "Collapse",
+    component: () => import("@/views/Collapse/CollapseContainer.vue"),
   },
-
-  {
-    path: '/expansion',
-    name: 'ExpansionPanels',
-    component: () => import('@/views/Expand/ExpansionPanels.vue'),
-  },
-]
-
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-})
+});
 
 // router.beforeEach((to,from,next)=>{
 
 // })
-export default router
+export default router;
