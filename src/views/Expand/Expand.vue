aa2b134359cdfd4b5136f3af7e70d5d28277bc21
Youssef Abdulaziz <azizy5203@gmail.com>
2024-08-22 10:27:02 +0300

    change

--- src/views/Expand/Expand.vue
+++ src/views/Expand/Expand.vue
@@ -1,27 +1,35 @@
 <script setup>
-import { ref } from "vue";
 import TableList from "../../components/shared/TableList";
-
 </script>
-
-
 
 <template>
   <div class="pa-5 h-100">
     <table-list>
-      <thead>
+      <!-- <thead>
         <v-row no-gutters tag="tr">
           <v-col v-for="n in 15" tag="th">long text header</v-col>
         </v-row>
-      </thead>
-      <tbody style="background-color: #00000000;">
+      </thead> -->
+      <tbody style="background-color: #00000000">
         <tr>
           <td>
             <v-expansion-panels multiple class="expand" variant="">
-              <v-expansion-panel v-for="n in 4" class="expand__panel" elevation="0" :ripple="false">
-                <v-expansion-panel-title class="expand__title" hide-actions :static="true" :ripple="false">
+              <v-expansion-panel
+                v-for="n in 4"
+                class="expand__panel"
+                elevation="0"
+                :ripple="false"
+              >
+                <v-expansion-panel-title
+                  class="expand__title"
+                  hide-actions
+                  :static="true"
+                  :ripple="false"
+                >
                   <v-row no-gutters>
-                    <v-col v-for="n in 15">long text header long text header long text header </v-col>
+                    <v-col v-for="n in 15"
+                      >long text header long text header long text header
+                    </v-col>
                   </v-row>
                 </v-expansion-panel-title>
                 <v-expansion-panel-text>
@@ -38,14 +46,13 @@ import TableList from "../../components/shared/TableList";
   </div>
 </template>
 <style lang="scss" scoped>
-$expand-color:#16707a;
+$expand-color: #16707a;
 .expand {
   &:hover {
     // background-color: transparent;
   }
 
   .v-expansion-panel-title__overlay {
-
     // background-color: green;
     :hover {
       // background-color: transparent;
@@ -57,10 +64,8 @@ $expand-color:#16707a;
     border: 1px solid transparent;
     border-radius: 16px;
 
-
     &.v-expansion-panel--active {
       border: 1px solid $expand-color;
-
     }
 
     :hover {
@@ -69,12 +74,10 @@ $expand-color:#16707a;
   }
 
   &__title {
-
     &.v-expansion-panel-title--active {
       background-color: $expand-color;
       color: white;
       border-radius: 16px 16px 0 0;
-
     }
 
     // background-color: red;
@@ -93,25 +96,23 @@ $expand-color:#16707a;
 
     .v-col {
       padding: 1rem 0;
-
     }
   }
 }
 
 table {
-
-  td {}
+  td {
+  }
 
   tbody {
     tr {
-      td {}
+      td {
+      }
     }
   }
 }
 
-
 .table-list {
-
   thead tr th:first-child {
     min-width: unset;
     width: unset;
@@ -133,7 +134,6 @@ table {
   }
 
   tbody {
-
     tr {
       &:hover {
         background-color: transparent;
@@ -146,11 +146,8 @@ table {
 
         &:first-child {
           padding: 0;
-
         }
-
       }
-
     }
   }
 }
--- src/views/Expand/ExpansionPanels.vue
+++ src/views/Expand/ExpansionPanels.vue
@@ -1,185 +1,209 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
-const model = ref()
-
+const model = ref();
 </script>
 
 <template>
-    <div class="pa-15">
-        <v-expansion-panels class="expansion" variant="accordion" v-model="model">
+  <div class="py-15 px-15">
+    <v-expansion-panels class="expansion" variant="accordion" v-model="model">
+      <div class="expansion__headers"></div>
+      <v-expansion-panel
+        v-for="n in 3"
+        elevation="0"
+        :value="`panel-${n}`"
+        :class="[
+          'expansion__panel',
+          model == `panel-${n}`
+            ? 'expansion__panel--expand'
+            : 'expansion__panel--collapse',
+        ]"
+        rounded="lg"
+      >
+        <v-expansion-panel-title
+          rounded="false"
+          :hide-actions="true"
+          :class="[
+            'expansion__title',
+            model == `panel-${n}`
+              ? 'expansion__title--expand'
+              : 'expansion__title--collapse',
+          ]"
+        >
+          <v-icon
+            :icon="
+              model == `panel-${n}` ? 'mdi-chevron-up' : 'mdi-chevron-down'
+            "
+            :color="model == `panel-${n}` ? 'white' : '#464E5F'"
+            class="me-4 mt-3"
+          />
+          <table class="title-table">
+            <thead>
+              <tr class="title-table__labels-row" v-if="n == 1">
+                <th>Risk ID</th>
+                <th>Risk Name</th>
+                <th>Risk Category</th>
+                <th>Risk Status</th>
+                <th>Risk Rate</th>
+              </tr>
+              <tr>
+                <th>0045</th>
+                <th>asdqwd</th>
+                <th>Reputational</th>
+                <th>Active</th>
+                <th>Severe</th>
+              </tr>
+            </thead>
+          </table>
+        </v-expansion-panel-title>
 
-            <div class="expansion__headers"></div>
-            <v-expansion-panel v-for="n in 3" elevation="0" :value="`panel-${n}`"
-                :class="['expansion__panel', model == `panel-${n}` ? 'expansion__panel--expand' : 'expansion__panel--collapse']"
-                rounded="lg">
-                <v-expansion-panel-title  rounded="false"
-                    :hide-actions="true"
-                    :class="['expansion__title', model == `panel-${n}` ? 'expansion__title--expand' : 'expansion__title--collapse']">
-                    <v-icon :icon="model == `panel-${n}` ? 'mdi-chevron-up' : 'mdi-chevron-down'"
-                        :color="model == `panel-${n}` ? 'white' : '#464E5F'" class="me-4 mt-3" />
-                    <table class="title-table">
-                        <thead>
-                            <tr class="title-table__labels-row" v-if="n == 1">
-                                <th>Risk ID</th>
-                                <th>Risk Name</th>
-                                <th>Risk Category</th>
-                                <th>Risk Status</th>
-                                <th>Risk Rate</th>
-                            </tr>
-                            <tr>
-                                <th>0045</th>
-                                <th>asdqwd</th>
-                                <th>Reputational</th>
-                                <th>Active</th>
-                                <th>Severe</th>
-                            </tr>
-                        </thead>
-                    </table>
-                </v-expansion-panel-title>
-
-                <v-expansion-panel-text class="expansion__body">
-                    <table class="content-table w-100">
-                        <thead>
-                            <tr>
-                                <th>Owner</th>
-                                <th>Caused by</th>
-                                <th>Mitigation Measures</th>
-                                <th>Impact</th>
-                                <th>Likelihood</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>AA</td>
-                                <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem neque repellat,
-                                    facere ullam id quibusdam.</td>
-                                <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem neque repellat,
-                                    facere ullam id quibusdam.</td>
-                                <td>
-                                    <span class="bg-grey-lighten-3 px-4 py-2 rounded-lg font-weight-medium">High</span>
-                                </td>
-                                <td>
-                                    <span class="bg-grey-lighten-3 px-4 py-2 rounded-lg font-weight-medium">High</span>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </v-expansion-panel-text>
-            </v-expansion-panel>
-        </v-expansion-panels>
-    </div>
+        <v-expansion-panel-text class="expansion__body">
+          <table class="content-table w-100">
+            <thead>
+              <tr>
+                <th>Owner</th>
+                <th>Caused by</th>
+                <th>Mitigation Measures</th>
+                <th>Impact</th>
+                <th>Likelihood</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>AA</td>
+                <td>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Exercitationem neque repellat, facere ullam id quibusdam.
+                </td>
+                <td>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Exercitationem neque repellat, facere ullam id quibusdam.
+                </td>
+                <td>
+                  <span
+                    class="bg-grey-lighten-3 px-4 py-2 rounded-lg font-weight-medium"
+                    >High</span
+                  >
+                </td>
+                <td>
+                  <span
+                    class="bg-grey-lighten-3 px-4 py-2 rounded-lg font-weight-medium"
+                    >High</span
+                  >
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </div>
 </template>
 
 <style lang="scss">
 .expansion {
+  &__headers {
+    background-color: #16707a;
+    width: 100%;
+    padding-block: 1.2rem;
+    border-radius: 0.625rem;
+  }
 
-    &__headers {
-        background-color: #16707A;
-        width: 100%;
-        padding-block: 1.2rem;
-        border-radius: .625rem;
+  &__panel {
+    border-width: 1px;
+
+    &--expand {
+      border-color: #16707a;
+      // border-radius: 0.625rem ;
     }
 
-    &__panel {
-        border-width: 1px;
+    &--collapse {
+      border-color: transparent;
+    }
+  }
 
-        &--expand {
-            border-color: #16707A;
-            // border-radius: 0.625rem ;
-        }
+  .v-expansion-panel {
+    // border-color: #16707A;
+    border-style: solid;
+  }
 
-        &--collapse {
-            border-color: transparent;
-        }
+  &__title {
+    // padding: 0;
+    padding-inline-start: 1rem;
+    font-size: 0.875rem;
+    color: white;
+
+    &--expand {
+      background-color: #16707a;
+      color: white;
     }
 
-    .v-expansion-panel {
-        // border-color: #16707A;
-        border-style: solid;
+    &--collapse {
+      background-color: transparent;
+      color: #464e5f;
     }
+  }
 
-
-    &__title {
-        // padding: 0;
-        padding-inline-start: 1rem;
-        font-size: 0.875rem;
-        color: white;
-        
-        &--expand {
-            background-color: #16707A;
-            color: white;
-        }
-        
-        &--collapse {
-            background-color: transparent;
-            color: #464E5F;
-        }
-
-    }
-
-    &__body {
-        padding-inline-start: .8rem;
-    }
+  &__body {
+    padding-inline-start: 0.8rem;
+  }
 }
 
-
 .title-table {
-    border-collapse: collapse;
-    width: 100%;
+  border-collapse: collapse;
+  width: 100%;
 
+  &__labels-row {
+    transform: translateY(-2.8rem);
+    background-color: #16707a;
+    color: #fff;
+  }
 
-    &__labels-row {
-        transform: translateY(-2.8rem);
-        background-color: #16707A;
-        color: #fff;
-    }
+  thead {
+    tr {
+      padding: 1rem;
 
-    thead {
-        tr {
-            padding: 1rem;
+      th {
+        text-align: start;
+        min-width: 7rem;
 
-            th {
-                text-align: start;
-                min-width: 7rem;
-
-                &:nth-of-type(1) {
-                    min-width: 4rem;
-                }
-
-                &:nth-of-type(2) {
-                    min-width: 15rem;
-                }
-            }
+        &:nth-of-type(1) {
+          min-width: 4rem;
         }
+
+        &:nth-of-type(2) {
+          min-width: 15rem;
+        }
+      }
     }
+  }
 }
 
 .content-table {
-    thead {
-        tr {
-            th {
-                padding-block: .5rem;
-                padding-inline-start: 1.5rem;
-                color: #B5B5C3;
-                ;
-            }
-        }
+  thead {
+    tr {
+      th {
+        padding-block: 0.5rem;
+        padding-inline-start: 1.5rem;
+        color: #b5b5c3;
+      }
     }
+  }
 
-    tbody {
-        tr {
-            vertical-align: middle;
+  tbody {
+    tr {
+      vertical-align: middle;
 
-            td {
-                padding-inline-start: 1.5rem;
-                color: #464E5F;
-            }
-        }
+      td {
+        padding-inline-start: 1.5rem;
+        color: #464e5f;
+      }
     }
+  }
 
-    th,
-    td {
-        text-align: start;
-    }
-}</style>
+  th,
+  td {
+    text-align: start;
+  }
+}
+</style>
